3907b23c7fba9eb5d48b6c155a3ec5f0c792536e
sudhakso <sonu.sudhakaran@gmail.com>
2024-09-16 19:59:02 +0530

    cert automation

--- scripts/certs/cert-gen.sh
+++ scripts/certs/cert-gen.sh
@@ -1,15 +1,10 @@
-
-# 1. Generate CA's private key and self-signed certificate
-openssl req -x509 -newkey rsa:4096 -days 365 -nodes -keyout ca-key.pem -out ca-cert.pem -subj "/C=IN/ST=KA/L=Bangalore/O=kinara/OU=kinops/CN=*.kinops.kinara.com/emailAddress=support-kinops@gmail.com"
-
-echo "CA's self-signed certificate"
-openssl x509 -in ca-cert.pem -noout -text
+#!/bin/bash
 
 # 2. Generate web server's private key and certificate signing request (CSR)
-openssl req -newkey rsa:4096 -nodes -keyout server-key.pem -out server-req.pem -subj "/C=IN/ST=KA/L=Bangalore/O=kinara/OU=kinops/CN=*.kinops.kinara.com/emailAddress=support-kinops@gmail.com"
+openssl req -newkey rsa:4096 -nodes -keyout ${CERT_PATH}/server-key.pem -out ${CERT_PATH}/server-req.pem -subj "/C=IN/ST=KA/L=Bangalore/O=kinara/OU=kinops/CN=*.kinops.kinara.com/emailAddress=support-kinops@gmail.com"
 
 # 3. Use CA's private key to sign web server's CSR and get back the signed certificate
-openssl x509 -req -in server-req.pem -days 60 -CA ca-cert.pem -CAkey ca-key.pem -CAcreateserial -out server-cert.pem -extfile server-ext.cnf
+openssl x509 -req -in ${CERT_PATH}/server-req.pem -days 60 -CA ${CA_PATH}/ca-cert.pem -CAkey ${CA_PATH}/ca-key.pem -CAcreateserial -out ${CERT_PATH}/server-cert.pem -extfile server-ext.cnf
 
 echo "Server's signed certificate"
-openssl x509 -in server-cert.pem -noout -text
+openssl x509 -in ${CERT_PATH}/server-cert.pem -noout -text
